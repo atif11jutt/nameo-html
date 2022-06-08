94d8c30ef7a2d59573ca20b6ef90dc2786d79c15
atif11jutt <atif11jutt@gmail.com>
2022-06-08 11:33:23 +0500

    upload

--- js/script.js
+++ js/script.js
@@ -8,4 +8,13 @@ jQuery(function($){
         e.preventDefault();
         $("#mobile-nav").removeClass("active")
     })
+
+    $("#play").click(function () {
+        $.cookie("name", $("#fname").val());
+        window.location.href = "video.html";
+    });
+
+    if($.cookie("name")){
+        $("#username").text($.cookie("name"))
+    }
 })
